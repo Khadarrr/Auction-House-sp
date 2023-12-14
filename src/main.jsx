c2d522b27c8d105fdc403206ac2cc80aed674c4b
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-12-14 17:49:17 +0100

    build fixs for netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "../src/root.js";
-import './app.css'
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
